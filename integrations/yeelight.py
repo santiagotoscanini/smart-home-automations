7524e7970930ee6b795a190a75cd9ae98cd26daf
Santiago Toscanini <github@account.toscanini.me>
2023-03-04 23:42:09 -0300

    [Automatically]: Update work-dotfiles.

--- integrations/yeelight.py
+++ integrations/yeelight.py
@@ -1,7 +1,7 @@
 from yeelight import Bulb
 
-monitor_ip = '192.168.1.11'
+monitor_ip = '192.168.1.9'
 monitor = Bulb(monitor_ip)
 
-desktop_ip = '192.168.1.3'
+desktop_ip = '192.168.1.11'
 desktop = Bulb(desktop_ip)
